ae1660a108f835176414d051af3e3755b1c5ae4e
Gage Eide <gageeide@gmail.com>
2021-05-20 17:49:29 -0500

    Gimme that commit baybeeeee

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>You can't convince me to tell you about my life, NextJS tutorial!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
